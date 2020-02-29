ad07fc4711921155adf42f18ead6760b632518aa
Parth-KaPatel <50957359+Parth-KaPatel@users.noreply.github.com>
2020-03-01 02:41:27 +0530

    URL Changed

--- app/src/main/java/com/example/resourcemanagementsystem/EndPoints.kt
+++ app/src/main/java/com/example/resourcemanagementsystem/EndPoints.kt
@@ -1,8 +1,8 @@
 package com.example.resourcemanagementsystem
 
 object EndPoints {
-    private var URL = "https://12b25fa9.ngrok.io/WebApi/v1/"
+    private var URL = "https://0dd29727.ngrok.io/WebApi/v1/"
     val URL_ADD_: String = URL + "addDept"
-    val URL_ADD_DEPT: String = "https://12b25fa9.ngrok.io/WebApi/v1/?op=addDept&dname=12&duname=12&dpwd=22"
+    val URL_ADD_DEPT: String = URL + "addDept"
     val URL_GET_DEPT: String = URL + "getDept"
 }
--- app/src/main/java/com/example/resourcemanagementsystem/Signup.kt
+++ app/src/main/java/com/example/resourcemanagementsystem/Signup.kt
@@ -6,18 +6,19 @@ import android.os.Handler
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.AuthFailureError
+import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_signup.*
 import org.json.JSONException
 import org.json.JSONObject
-import java.util.*
 
 
 class signup : AppCompatActivity() {
 
- private var URL = "http://192.168.1.7/WebApi/v1/?op="
+    private var URL = "http://192.168.1.7/WebApi/v1/?op="
     val URL_ADD_DEPT: String = URL + "addDept"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,7 +73,7 @@ class signup : AppCompatActivity() {
         val dpwd = rpwd.text.toString()
 
 
-        val stringRequest = object : StringRequest(
+        /*   val stringRequest = object : StringRequest(
             Method.POST,
             EndPoints.URL_ADD_DEPT, Response.Listener<String>() { response ->
                 try {
@@ -106,6 +107,44 @@ class signup : AppCompatActivity() {
 
         VolleySingleton.instance?.addToRequestQueue(stringRequest)
         //Toast.makeText(this, "$dname $duname $dpwd", Toast.LENGTH_LONG)
-        Toast.makeText(this, stringRequest.toString(), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, stringRequest.toString(), Toast.LENGTH_LONG).show()*/
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://0dd29727.ngrok.io/WebApi/v1/?op=addDept"
+
+// Request a string response from the provided URL.
+        val stringRequest = object : StringRequest(Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                try {
+                    val obj = JSONObject(response)
+
+                    Toast.makeText(
+                        applicationContext,
+                        obj.getString("message"),
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener() { volleyError ->
+                Toast.makeText(applicationContext, volleyError.message, Toast.LENGTH_LONG).show()
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params["dname"] = dname
+                params["duname"] = duname
+                params["dpwd"] = dpwd
+                return params
+
+            }
+        }
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
+
     }
 }
